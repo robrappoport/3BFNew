2feb9ad7f11f33acfcfa0d9f6b06daf6f112dbd0
Robert Rappoport <rjr363@nyu.edu>
2018-05-10 02:14:08 -0400

    Putting together initial set up

---
+++ Three Bags Full/Assets/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    public List<GameObject> actors;
+    // Use this for initialization
+    void Awake () {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        actors = new List<GameObject>(GameObject.FindGameObjectsWithTag("Actor"));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ActorScaling();
+
+    }
+
+    void ActorScaling()
+    {
+        foreach (GameObject actor in actors)
+        {
+
+            Vector3 screenLoc = Camera.main.WorldToScreenPoint(new Vector3(actor.transform.position.x, actor.transform.position.y, 0f));
+            actor.transform.localScale = new Vector2(Mathf.Clamp(screenLoc.y/100f, 1f, 5f), Mathf.Clamp(screenLoc.y/100f,1f, 5f));
+        }
+    }
+}
---
+++ Three Bags Full/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+
+    public Vector3 targetPos;
+    public KeyCode clickToMove, clickToInteract;
+    public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerMovement();
+
+    }
+
+    void PlayerMovement()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
+        if (Input.GetKeyDown(clickToMove))
+        {
+            targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * moveSpeed);
+    }
+
+}
